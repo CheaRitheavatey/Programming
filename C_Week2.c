69de68b8c9bc13ea136ee45d1251888d52e7def5
CheaRitheavatey <2023447chea@aupp.edu.kh>
2024-09-23 13:59:12 +0200

    Update C_Week2.c

--- C_Week2.c
+++ C_Week2.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
-int main() {
-    unsigned char ch = 'A';
-    printf("char: %C\n", ch);
-    printf("char: ")
+
+int main()
+{
+    unsigned char a = 'A';
+    printf("%C\n",a);
+    printf("%u\n",a); // print out ascii of
+    // ..........................
+    
+    char ch;
+    
+    printf("enter char (press enter to finish): \n");
+    while ((ch = getchar()) != '\n') {
+        printf("%C",ch);
+    }
+    printf("\neneter detected, finish");
+    
+    //.............................
+    
+    char input[100]; // array
+    printf("enter string: ");
+    scanf("%99s", input); // 99s mean its max length
+    printf("you enter: %s\n", input);
+    return 0;
+    
+    // putchar prints one character whereas printf can print a lot more
+
+    // when theres an overflow in char 255 is the max. and when we put 256 it will loop back to number 1,2,3...
 }
