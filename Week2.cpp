df85ece70b688aebbd89e9658eccf81d68df9634
CheaRitheavatey <2023447chea@aupp.edu.kh>
2024-10-07 13:02:30 +0200

    Update Week2.cpp

--- Week2.cpp
+++ Week2.cpp
@@ -2,6 +2,137 @@
 
 using namespace std;
 
+// week 5
+
+int prac3While() {
+    int i;
+    char x;
+    // ask input
+    // cout << "Enter number: ";
+    // cin >> i;
+    
+    while (true) {
+        cout << "enter number: ";
+        cin >> i;
+        
+        if (i > 0) {
+            cout << "you entered: " << i << endl;
+        } else {
+            cout << "invalid input" << endl;
+            continue;
+        }
+
+        // ask for input again
+        cout << "Do you want to input again? (y/n): ";
+        cin >> x;
+
+        if (x == 'y')
+        {
+            continue;
+        } else {
+            cout << "Program finished";
+            break;
+        }
+        
+    }
+    return 0;
+}
+
+int pracDoWhile() {
+    int i;
+    do {
+        cout << "enter positive number: ";
+        cin >> i;
+    } while (i < 0);
+    
+    cout << "you entered: " << i;
+    
+
+    return 0;
+}
+int practWhileLoop() {
+    int i;
+    // ask input
+    cout << "Enter number: ";
+    cin >> i;
+    
+    
+    while (i<= 0) {
+        
+        cout << "invalid input" << endl;
+        cout << "Enter number: ";
+        cin >> i;
+    }
+    
+    cout << "you entered: " << i ;
+
+    return 0;
+}
+// do wile and while loop
+void week5() {
+    int num, i =0;
+
+    // input
+    cout << "enter a number: ";
+    cin >> num;
+
+    cout << "do while: " << endl;
+    do
+    {
+        cout << i << " ";
+        i++;
+    } while (i < num);
+
+    
+    // do while will do the code in the 'do'no matter what
+
+
+    // while loop will stop if theres an error
+    
+    
+    
+}
+
+// for loop is ideal for when you know how many iteration
+void forloop() {
+    double a;
+    double b;
+    double c;
+    
+    cout << "Enter a b c: ";
+    cin >> a >> b >> c;
+    
+    if (a >= c >= b) {
+        cout << "c is in an interval of " << a << "and" << b; 
+    }
+
+
+    for (int i = 1; i <=5; i++) {
+        for (int j = 1; j<=i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+
+        
+    }
+    for (int x = 1; x <=5; x++) {
+        for (int j = 5; j >= x; j-- ) {
+                cout << "* ";
+            } 
+            cout << endl;
+    }
+
+   for (int i = 1; i <= 4; i++) {
+        for (int j = 4; j >= i; j--)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+        
+    }
+    
+}
+
 void month() {
      // example 3
     int month;
@@ -115,8 +246,14 @@ void postiveORnegative(){
         
     }
 }
+
     // main method
 int main() {
+    prac3While();
+    // pracDoWhile();
+    // practWhileLoop();
+    // week5();
+    // forloop();
     // month();
     // season();
     // triangle();
